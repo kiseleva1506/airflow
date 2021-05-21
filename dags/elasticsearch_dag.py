9c653cf19e9bc2109e3bb3a963c9123dc0fad66a
Mary <kiseleva15@gmail.com>
2021-05-21 15:52:04 +0000

    elasticsearch hook

---
+++ dags/elasticsearch_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from elasticsearch_plugin.hooks.elastic_hook import ElasticHook
+
+from datetime import datetime
+
+default_args = {"start_date": datetime(2021, 1, 1)}
+
+def _print_es_info():
+    hook = ElasticHook()
+    print(hook.info())
+
+with DAG("elasticsearch_dag",
+         schedule_interval="@daily",
+         default_args=default_args,
+         catchup=False) as dag:
+    
+   print_es_info = PythonOperator(
+       task_id="print_es_info",
+       python_callable=_print_es_info
+   )
